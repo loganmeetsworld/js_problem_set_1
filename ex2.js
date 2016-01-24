b6592a0f9d5fd34f4c24c54ede860f74319adf9d
Logan McDonald <loganmcdona11@gmail.com>
2016-01-24 10:35:56 -0800

    finished ex2

---
+++ ex2.js
@@ -0,0 +1,15 @@
+var pet = "cat"
+var food = "ice cream"
+
+if (pet === "cat")
+  console.log("here kitty");
+else if (pet === "dog")
+  console.log("woof");
+else 
+  console.log("some other sound")
+
+if (food === "broccoli")
+  console.log("eh.");
+else if (food === "ice cream")
+  console.log("yum");
+
