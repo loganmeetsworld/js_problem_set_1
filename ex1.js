d681eabe5946a1f8b1fa3868ccfe8352376e5a00
Logan McDonald <loganmcdona11@gmail.com>
2016-01-24 10:34:04 -0800

    ex1 is complete

---
+++ ex1.js
@@ -0,0 +1,9 @@
+var person_age = 55
+var ada_age = 2
+
+if (person_age < ada_age)
+  console.log("This person is younger");
+else if (ada_age < person_age)
+  console.log("Ada is younger");
+else
+  console.log("They're the same!");
